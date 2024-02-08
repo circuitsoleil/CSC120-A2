b2c29023d1a62a734330bfeefda200b66ae73218
Merry <mwieland@smith.edu>
2024-02-07 21:45:44 -0500

    Work as of Wednesday

--- computer.py
+++ computer.py
@@ -1,10 +1,19 @@
 class Computer:
 
-    # What attributes will it need?
-
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
+    #construst computer object with attributes
+    def __init__(self, 
+                 description, 
+                 processor_type, 
+                 hard_drive_capacity, 
+                 computer_memory, 
+                 operating_system, 
+                 year_made, 
+                 price):
+        
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.computer_memory = computer_memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,9 @@
+from computer import *
+
 class ResaleShop:
 
-    # What attributes will it need?
+    inventory = []
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
-    # What methods will you need?
+    def __init__(self,computer):
+        self.inventory.append(computer)
+        print(self.inventory)
